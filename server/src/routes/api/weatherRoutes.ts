14ed79225214cb6bccb2172b1e724cb5dc767983
WillWhittakerDHP <will@districthomepro.com>
2024-11-14 11:18:02 -0500

    catch-up commit

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -5,15 +5,47 @@ const router = Router();
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
+//23/mod/ser/src/rout/tiproutes
 router.post('/', (req: Request, res: Response) => {
+  // // This API route ("/api/tips") is a POST Route for a new UX/UI tip
+  // router.post('/', async (req: Request, res: Response) => {
+  //   const { username, topic, tip } = req.body;
+  //   if (req.body) {
+  //     await TipService.addTip(username, topic, tip);
+  //     res.json('Tip added successfully');
+  //   } else {
+  //     res.send('Error in adding tip');
+  //   }
+  // });
   // TODO: GET weather data from city name
+    //router.get('/history', async (req: Request, res: Response) => {});
+//     // This API route ("/api/tips") is a GET Route for retrieving all the tips
+    // router.get('/', async (req: Request, res: Response) => {
+    //   console.info(`${req.method} request received for tips`);
+    //   const data = await TipService.getTips();
+    //   res.json(data);
+    // });
   // TODO: save city to search history
+    //router.delete('/history/:id', async (req: Request, res: Response) => {});
+});
+
+// This API route ("/api/tips") is a GET Route for retrieving all the tips
+router.get('/', async (req: Request, res: Response) => {
+  console.info(`${req.method} request received for tips`);
+  const data = await TipService.getTips();
+  res.json(data);
 });
 
 // TODO: GET search history
 router.get('/history', async (req: Request, res: Response) => {});
+  // This API route ("/api/tips") is a GET Route for retrieving all the tips
+    // router.get('/', async (req: Request, res: Response) => {
+    //   console.info(`${req.method} request received for tips`);
+    //   const data = await TipService.getTips();
+    //   res.json(data);
+    // });
 
-// * BONUS TODO: DELETE city from search history
+// TODO: DELETE city from search history
 router.delete('/history/:id', async (req: Request, res: Response) => {});
 
 export default router;
--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,10 +1,21 @@
-import path from 'node:path';
+//!dropped in
+import express, { type Request, type Response } from 'express';import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { Router } from 'express';
+// import { Router } from 'express';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const router = Router();
+// const router = Router();
+//!This is the file that needed editing
+// // TODO: Define route to serve index.html
+// 25/server/src/routes/htmlRoutes
+const router = express.Router();
 
-// TODO: Define route to serve index.html
+router.get('/', (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+});
+
+router.get('/feedback', (_req: Request, res: Response) =>
+  res.sendFile(path.join(__dirname, '../../../client/dist/feedback.html'))
+);
 
 export default router;
